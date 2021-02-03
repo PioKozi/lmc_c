6be23ec7d2865cf99e11caf5dc77dc41231ad515
piokozi <47647338+PioKozi@users.noreply.github.com>
2021-02-03 15:40:26 +0000

    Minor edits:
    - Removed unnecessary variables
    - Renamed "variables" to "pointers"
    - Minor formatting
    - Turned -Wall back on in Makefile

--- main.c
+++ main.c
@@ -3,28 +3,27 @@
 #include <string.h>
 
 const int ramsize = 99;
-const int maxsize = 30;
 struct entry {
-    char* str;
-    int  n;
+    char *str;
+    int   n;
 };
-// allow people to use variable names, ugly solution but should work
-struct entry variables[ramsize] = {};  // map var name to var location
-struct entry instructions[]     = {
-    "LDA", 5,    // load
-    "STA", 3,    // store
-    "ADD", 1,    // add
-    "SUB", 2,    // subtract
-    "INP", 901,  // input
-    "OUT", 902,  // output
-    "END", 0,    // halt
-    "BRZ", 7,    // branch if zero
-    "BRP", 8,    // branch if positive
-    "BRA", 6,    // always branch
-    "DAT", 0,    // data/variable
+// map pointer/var name to pointer location/line
+struct entry       pointers[ramsize] = {};
+const struct entry instructions[]    = {
+    {"LDA", 5},    // load
+    {"STA", 3},    // store
+    {"ADD", 1},    // add
+    {"SUB", 2},    // subtract
+    {"INP", 901},  // input
+    {"OUT", 902},  // output
+    {"END", 0},    // halt
+    {"BRZ", 7},    // branch if zero
+    {"BRP", 8},    // branch if positive
+    {"BRA", 6},    // always branch
+    {"DAT", 0},    // data/variable
 };
 // return int if found, -1 if not found
-int num_for_instr(char *instr)
+int code_for_instr(char *instr)
 {
     int n_instructions = sizeof(instructions) / sizeof(instructions[0]);
     for (int i = 0; i < n_instructions; i++)
@@ -32,12 +31,12 @@ int num_for_instr(char *instr)
             return instructions[i].n;
     return -1;
 }
-int loc_for_var(char *varname)
+int loc_for_pointer(char *varname)
 {
-    int n_vars = sizeof(variables) / sizeof(variables[0]);
+    int n_vars = sizeof(pointers) / sizeof(pointers[0]);
     for (int i = 0; i < n_vars; i++)
-        if (strcmp(varname, variables[i].str) == 0)
-            return variables[i].n;
+        if (strcmp(varname, pointers[i].str) == 0)
+            return pointers[i].n;
     return -1;
 }
 
@@ -58,6 +57,5 @@ int main()
     /* int   mdr = 0;       // memory data register */
     /* char *cir;           // current instruction register */
     load_data(ram);
-    /* printf("%s %d\n", variables[0].str, variables[0].n); */
     return 0;
 }
